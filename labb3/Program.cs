1938e88b9d47c80e56c2abb948c9123524bb5b10
Greed <106820018+maxberglund2@users.noreply.github.com>
2024-12-16 12:09:01 +0100

    getStudents func

--- labb3/Program.cs
+++ labb3/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace labb3
+﻿using labb3.Data;
+
+namespace labb3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Labb3");
+            getStudents();
+
+        }
+        static void getStudents()
+        {
+            using (var context = new SchoolDBContext())
+            {
+                var employees = context.Students.Select(e => e);
+                Console.Clear();
+                Console.WriteLine("Students:");
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine($"\n{employee.FirstName} {employee.LastName}");
+                }
+            }
         }
     }
 }
